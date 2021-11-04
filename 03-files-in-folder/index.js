9c9ebdf75020999a854e39041b372d134259b699
Andrey Peshko <coursestud@mail.ru>
2021-11-04 19:14:47 +0300

    feat: solve files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const fs = require('fs/promises');
+
+const pathToFolder = path.join(__dirname, 'secret-folder');
+
+readFolder(pathToFolder);
+
+async function readFolder(pathToFolder) {
+  try {
+    const files = await fs.readdir(pathToFolder, {withFileTypes: true});
+    printInfoAboutFiles(files, pathToFolder);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function printInfoAboutFiles(files, pathToFolder) {  
+  files.forEach(async file => {
+    const fullNameFile = file.name;
+    const pathToFile = path.join(pathToFolder, fullNameFile);
+    if (file.isFile()) {
+      const fileInfo = await fs.stat(pathToFile);
+      const nameFile = getFileName(fullNameFile);
+      const extFile = getFileExt(pathToFile);
+      const sizeFile = getSizeFile(fileInfo);
+      console.log(nameFile + ' - ' + extFile +' - ' + sizeFile);
+    }
+  });
+}
+
+function getSizeFile(fileInfo) {
+  const size = Math.floor(fileInfo.size / 1000) + ' kb';
+  return size;
+}
+
+function getFileName(fullName) {
+  const nameFile = fullName.split('.')[0];
+  return nameFile;
+}
+
+function getFileExt(pathToFile) {
+  const extFile = path.extname(pathToFile);
+  return extFile.split('.').join('');
+}
